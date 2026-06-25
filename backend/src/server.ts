a1f54f288bebcb3aca937b15fed995e255e536a2
Rezwan66 <shaikhrezwan66@gmail.com>
2026-06-25 19:53:22 +0200

    Initial commit for full project

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app from './app';
+
+const port = process.env.PORT || 5000; // The port your express server will be running on.
+
+(function bootstrap() {
+  try {
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+})();
